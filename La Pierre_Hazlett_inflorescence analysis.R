4cebebc36d6941c374fb46c6b26acebc09950e28
klapierre <lapierre.kimberly@gmail.com>
2016-07-16 10:57:46 -0500

    Import data from xlsx files.

--- La Pierre_Hazlett_inflorescence analysis.R
+++ La Pierre_Hazlett_inflorescence analysis.R
@@ -1,3 +1,4 @@
+library(openxlsx)
 library(nlme)
 library(ggplot2)
 library(grid)
@@ -11,15 +12,16 @@ setwd('C:\\Users\\Kim\\Dropbox\\2015_NSF_LaPierre\\data\\Hazlett data\\fwinflore
 ###################################################
 
 theme_set(theme_bw())
-theme_update(axis.title.x=element_text(size=20, vjust=-0.35, margin=margin(t=15)), axis.text.x=element_text(size=16),
-             axis.title.y=element_text(size=20, angle=90, vjust=0.5, margin=margin(r=15)), axis.text.y=element_text(size=16),
+theme_update(axis.title.x=element_text(size=20, vjust=-0.35, margin=margin(t=15)),
+             axis.text.x=element_text(size=16),
+             axis.title.y=element_text(size=20, angle=90, vjust=0.5, margin=margin(r=15)),
+             axis.text.y=element_text(size=16),
              plot.title = element_text(size=24, vjust=2),
              panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
              legend.title=element_blank(), legend.text=element_text(size=20))
 
 ###bar graph summary statistics function
 #barGraphStats(data=, variable="", byFactorNames=c(""))
-
 barGraphStats <- function(data, variable, byFactorNames) {
   count <- length(byFactorNames)
   N <- aggregate(data[[variable]], data[byFactorNames], FUN=length)
@@ -37,7 +39,34 @@ barGraphStats <- function(data, variable, byFactorNames) {
   return(finalSummaryStats)
 }  
 
+###################################################
+###################################################
 
-###################################################
-###################################################
+#read in data
+achi2016 <- read.xlsx('Hazlett_Achillea_ReproductiveCounts_July.xlsx', sheet='July 7')
+agro2016 <- read.xlsx('Hazlett_Agro_ReproductiveCounts_July.xlsx', sheet='July 5')
+amor2016 <- read.xlsx('Hazlett_Amoca_ReproductiveCounts_July.xlsx', sheet='July 7')
+anem2016 <- read.xlsx('Hazlett_Anenome_ReproductiveCounts_July.xlsx', sheet='July 5')
+ascl2016 <- read.xlsx('Hazlett_Asctu_ReproductiveCounts_July.xlsx', sheet='July 12')
+lupi2016 <- read.xlsx('Hazlett_LupineSeedCount_June.xlsx', sheet='June 21')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
